45b9d03df0bd54758dc815b7e6073eab227376f0
Frankee.zhou <frankee.zhou@gmail.com>
2022-05-26 10:25:04 +0800

    add CheckVersion method for OpenAPI

--- go/pkg/mojo/openapi/openapi.go
+++ go/pkg/mojo/openapi/openapi.go
@@ -1,39 +1,62 @@
 package openapi
 
 import (
-    "github.com/mojo-lang/core/go/pkg/mojo/core"
-    "reflect"
-    "strings"
+	"github.com/mojo-lang/core/go/pkg/mojo/core"
+	"reflect"
+	"strings"
 )
 
 func New() *OpenAPI {
-    return &OpenAPI{
-        Openapi: &core.Version{
-            Major: 3,
-            Minor: 0,
-            Patch: 3,
-        },
-        Info:       &Info{},
-        Paths:      &Paths{Vals: map[string]*PathItem{}},
-        Components: &Components{Schemas: map[string]*Schema{}},
-    }
+	return &OpenAPI{
+		Openapi: &core.Version{
+			Major: 3,
+			Minor: 0,
+			Patch: 3,
+		},
+		Info:       &Info{},
+		Paths:      &Paths{Vals: map[string]*PathItem{}},
+		Components: &Components{Schemas: map[string]*Schema{}},
+	}
+}
+
+// CheckVersion check the version is "3.0", or "3.1"
+func (x *OpenAPI) CheckVersion(version string) bool {
+	if x != nil && x.Openapi != nil {
+		v, err := core.ParseVersion(version)
+		if err != nil {
+			return false
+		}
+
+		if v.Level > 0 && v.Major != x.Openapi.Major {
+			return false
+		}
+		if v.Level > 1 && v.Minor != x.Openapi.Minor {
+			return false
+		}
+		if v.Level > 2 && v.Patch != x.Openapi.Patch {
+			return false
+		}
+
+		return true
+	}
+	return false
 }
 
 func (x *OpenAPI) GetOperation(id string) (path string, method string, operation *Operation, err error) {
-    if x != nil && x.Paths != nil {
-        for k, v := range x.Paths.Vals {
-            for _, method = range []string{"Get", "Put", "Post", "Delete", "Options", "Head", "Patch", "Trace"} {
-                ok := false
-                op := reflect.ValueOf(v).Elem().FieldByName(method)
-                if !op.IsNil() {
-                    if operation, ok = op.Interface().(*Operation); ok && operation.OperationId == id {
-                        path = k
-                        method = strings.ToUpper(method)
-                        return
-                    }
-                }
-            }
-        }
-    }
-    return "", "", nil, core.NewNotFoundError("id: %s", id)
+	if x != nil && x.Paths != nil {
+		for k, v := range x.Paths.Vals {
+			for _, method = range []string{"Get", "Put", "Post", "Delete", "Options", "Head", "Patch", "Trace"} {
+				ok := false
+				op := reflect.ValueOf(v).Elem().FieldByName(method)
+				if !op.IsNil() {
+					if operation, ok = op.Interface().(*Operation); ok && operation.OperationId == id {
+						path = k
+						method = strings.ToUpper(method)
+						return
+					}
+				}
+			}
+		}
+	}
+	return "", "", nil, core.NewNotFoundError("id: %s", id)
 }
--- go/pkg/mojo/openapi/openapi_test.go
+++ go/pkg/mojo/openapi/openapi_test.go
@@ -1,25 +1,39 @@
 package openapi
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/mojo-lang/core/go/pkg/mojo/core"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestOpenAPI_GetOperation(t *testing.T) {
-    o := New()
-    o.Paths.Vals["/path/to/service"] = &PathItem{
-        Get: &Operation{
-            OperationId: "test_operation_id",
-            Summary:     "test_operation_summary",
-        },
-        Post: &Operation{
-            OperationId: "test_operation_id_2",
-            Summary:     "test_operation_2_summary",
-        }}
+	o := New()
+	o.Paths.Vals["/path/to/service"] = &PathItem{
+		Get: &Operation{
+			OperationId: "test_operation_id",
+			Summary:     "test_operation_summary",
+		},
+		Post: &Operation{
+			OperationId: "test_operation_id_2",
+			Summary:     "test_operation_2_summary",
+		}}
 
-    p, m, api, e := o.GetOperation("test_operation_id")
-    assert.NoError(t, e)
-    assert.Equal(t, "/path/to/service", p)
-    assert.Equal(t, "GET", m)
-    assert.Equal(t, "test_operation_summary", api.Summary)
+	p, m, api, e := o.GetOperation("test_operation_id")
+	assert.NoError(t, e)
+	assert.Equal(t, "/path/to/service", p)
+	assert.Equal(t, "GET", m)
+	assert.Equal(t, "test_operation_summary", api.Summary)
+}
+
+func TestOpenAPI_IsVersion(t *testing.T) {
+	o := New()
+	o.Openapi = &core.Version{Major: 3, Minor: 0, Patch: 3}
+
+	assert.True(t, o.CheckVersion("3"))
+	assert.True(t, o.CheckVersion("3.0"))
+	assert.True(t, o.CheckVersion("3.0.3"))
+
+	assert.False(t, o.CheckVersion("2"))
+	assert.False(t, o.CheckVersion("3.1"))
+	assert.False(t, o.CheckVersion("3.0.0"))
 }
